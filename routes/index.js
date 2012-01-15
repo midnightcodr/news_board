f3a43b9bc990c311abe35510ada6c65e43ecba70
zc <zc@ubuntu.(none)>
2012-01-15 03:13:46 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.news_pub = function(req, res){
+  var user_role=req.param('key')=='123abc'? 'admin': 'operator';
+  res.render('news_pub', { 
+	title: 'News Board', 
+	user_role: user_role,
+	news_items:[ "what's up everyone?", "how's last night's sleep", "I have back pain.", "lovely day" ] })
+};
